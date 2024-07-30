279c12efce876752f6531820c42de81f4ab07f3e
Arul Nadesan <annadesa@amazon.com>
2024-07-30 23:40:16 +0530

    twilio questions

---
+++ Misc/HTTPRequests.java
@@ -0,0 +1,7 @@
+package Misc;
+
+/*
+https://www.twilio.com/en-us/blog/5-ways-to-make-http-requests-in-java
+ */
+public class HTTPRequests {
+}
---
+++ Strings/PrefixSuffixMatchingTwilio.java
@@ -0,0 +1,16 @@
+package Strings;
+
+
+/*
+text - banana
+prefix - bana
+suffix - nana
+output - banana
+
+text - nothing
+prefix - asdfno - no
+suffix - ingenous -ing
+ouput - nothing
+ */
+public class PrefixSuffixMatchingTwilio {
+}
