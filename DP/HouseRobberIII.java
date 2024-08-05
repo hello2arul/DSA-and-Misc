a50bc68bbcd0d4a4fcc6eaced640b5b787d42d9e
Arul Nadesan <annadesa@amazon.com>
2024-08-05 20:27:04 +0530

    add

---
+++ DP/HouseRobberIII.java
@@ -0,0 +1,23 @@
+package DP;
+
+
+/*
+    https://leetcode.com/problems/house-robber-iii
+ */
+public class HouseRobberIII {
+    Map<TreeNode, Integer> map = new HashMap<>();
+
+    public int rob(TreeNode root) {
+        if(root == null)
+            return 0;
+        if (map.containsKey(root))
+            return map.get(root);
+        int robbingCurrentRoot = root.val +
+                (root.left != null ? (rob(root.left.left) + rob(root.left.right)) : 0) +
+                (root.right != null ? (rob(root.right.left) + rob(root.right.right)) : 0);
+        int notRobbingCurrent = rob(root.left) + rob(root.right);
+        int res = Math.max(robbingCurrentRoot, notRobbingCurrent);
+        map.put(root, res);
+        return res;
+    }
+}
