abac3a228d5d51704b970308bc12d41af1e62a55
hello2arul <hello2arul@gmail.com>
2025-01-28 22:53:43 +0530

    add

--- BackTracking/VerbalArithmeticPuzzle.java
+++ BackTracking/VerbalArithmeticPuzzle.java
@@ -2,10 +2,11 @@ package BackTracking;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
 
 /*
- * https://leetcode.com/problems/verbal-arithmetic-puzzle
- * Google
+https://leetcode.com/problems/verbal-arithmetic-puzzle
+Google
 
 Given an equation, represented by words on the left side and the result on the right side.
 
@@ -23,72 +24,74 @@ Explanation: Map 'S'-> 9, 'E'->5, 'N'->6, 'D'->7, 'M'->1, 'O'->0, 'R'->8, 'Y'->'
 Such that: "SEND" + "MORE" = "MONEY" ,  9567 + 1085 = 10652
  */
 public class VerbalArithmeticPuzzle {
-    // TC - O(10n⋅m)
+    // TC - O(10^n⋅m)
     // n -> number of chars
     // m -> TC of doesMatch
     // SC - > O(n), recursive call stack depth
     public boolean isSolvable(String[] words, String result) {
-        HashSet<Character> uniqueChars = new HashSet<>();
-        HashSet<Character> startChars = new HashSet<>();
+        Set<Character> uniqueChars = new HashSet<>();
+        Set<Character> firstChars = new HashSet<>();
 
         for (String word : words) {
             for (int i = 0; i < word.length(); i++) {
                 char ch = word.charAt(i);
-                if (i == 0 && word.length() != 1)
-                    startChars.add(ch);
                 uniqueChars.add(ch);
+
+                if (i == 0 && word.length() != 1)
+                    firstChars.add(ch);
             }
         }
-        if (result.length() != 0)
-            startChars.add(result.charAt(0));
-        for (char ch : result.toCharArray()) {
+
+        for (int i = 0; i < result.length(); i++) {
+            char ch = result.charAt(i);
             uniqueChars.add(ch);
+
+            if (i == 0)
+                firstChars.add(ch);
         }
 
-        if (uniqueChars.size() > 10) return false;
-        // if unique char is 1, map it to 0
-        // [A, A] = "AA" -> false
-        if (uniqueChars.size() == 1 && result.length() == 1) return true;
-
+        if (uniqueChars.size() > 10)
+            return false;
+        // AA -> A, can be 0
+        // AA -> AA, can't be 0
+        if (uniqueChars.size() == 1 && result.length() == 1)
+            return true;
+        
         Character[] chars = uniqueChars.toArray(new Character[0]);
-        boolean[] used = new boolean[10]; // Track used digits
-        int[] mapping = new int[128]; // Map characters to digits
+        boolean[] used = new boolean[10];
+        int[] mapping = new int[128];
 
-        return backtrack(chars, startChars, mapping, used, 0, words, result);
+        return backtract(chars, firstChars, used, mapping, 0, words, result);
     }
 
-    private boolean backtrack(Character[] chars, HashSet<Character> startChars, int[] mapping, boolean[] used, int idx, String[] words, String result) {
+    private boolean backtract(Character[] chars, Set<Character> firstChars, boolean[] used, int[] mapping, int idx, String[] words, String result) {
         if (idx == chars.length) {
             return doesMatch(mapping, words, result);
         }
-
         char ch = chars[idx];
+
         for (int digit = 0; digit <= 9; digit++) {
-            // Skip if the digit is already used
-            if (used[digit]) continue;
+            if (used[digit])
+                continue;
+            if (digit == 0 && firstChars.contains(ch))
+                continue;
 
-            // Skip if this character cannot be mapped to zero
-            if (digit == 0 && startChars.contains(ch) ) continue;
-
-            // Set mapping
-            mapping[ch] = digit;
             used[digit] = true;
+            mapping[ch] = digit;
 
-            // Recur to map the next character
-            if (backtrack(chars, startChars, mapping, used, idx + 1, words, result)) {
+            if (backtract(chars, firstChars, used, mapping, idx + 1, words, result)) {
                 return true;
             }
 
-            // Backtrack
             used[digit] = false;
-            mapping[ch] = 0; // Reset mapping
+            mapping[ch] = 0;
         }
         return false;
     }
 
-    private boolean doesMatch(int[] mapping, String[] words, String result) {
+    public boolean doesMatch(int[] mapping, String[] words, String res) {
         int sum = 0;
-        int resultSum = convertToNum(result, mapping);
+        int resultSum = convertToNum(res, mapping);
         for (String word : words) {
             sum += convertToNum(word, mapping);
             if (sum > resultSum)
@@ -97,9 +100,9 @@ public class VerbalArithmeticPuzzle {
         return sum == resultSum;
     }
 
-    private int convertToNum(int[] mapping, String s) {
+    private int convertToNum(String word, int[] mapping) {
         int num = 0;
-        for (char ch : s.toCharArray()) {
+        for (char ch : word.toCharArray()) {
             num = (num * 10) + mapping[ch];
         }
         return num;
